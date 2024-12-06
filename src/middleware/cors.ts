31d6cadd40bb8ac88bd89d9ae5370360fde512ae
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-06 22:16:06 +0700

    Fix cors error

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,6 +2,6 @@ import { cors } from 'hono/cors';
 
 export const corsMiddleware = cors({
   origin: '*',
-  allowMethods: ['GET', 'POST', 'OPTIONS'],
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowHeaders: ['Content-Type', 'Authorization'],
 });
