c83d51ab121411ac5be93650649bd464017bea94
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-06 22:16:52 +0700

    Added new routes for user management by admin

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { createSupabaseClient } from '../services/supabase';
+import { adminSupabaseClient, createSupabaseClient } from '../services/supabase';
 
 const user = new Hono();
 
@@ -27,10 +27,96 @@ user.post('/adduser', async (c) => {
       return c.json({ message: 'Error inserting user data', error: upsertError.message }, 400);
     }
 
-    console.log("Successfull signed up");
-    console.log({ user_id, email, username, phone, created_at, updated_at });
     return c.json({ user_id, email, username, phone, created_at, updated_at });
   } catch (error) {
+    console.error('Unexpected error:', error);
+    return c.json({ message: 'Unexpected error', error: error }, 500);
+  }
+});
+
+// Fetch all users from the database
+user.get('/users', async (c) => {
+  const supabase = createSupabaseClient(c);
+
+  try {
+    const { data, error } = await supabase
+      .from('users')
+      .select('*')
+
+    if (error) {
+      return c.json({ message: 'Error fetching users', error: error.message }, 400);
+    }
+
+    return c.json(data);
+  } catch (error) {
+    return c.json({ message: 'Unexpected error', error: error }, 500);
+  }
+});
+
+// Update user by Admin route
+user.put('/updateuser', async (c) => {
+  const supabase = createSupabaseClient(c);
+  const adminSupabase = adminSupabaseClient(c);
+  const { user_id, email, username, phone } = await c.req.json();
+  const updated_at = new Date();
+
+  try {
+    // Update user in the auth.users table (supabase authentication)
+    const { error: authError } = await adminSupabase.auth.admin.updateUserById(
+      user_id,
+      { email }
+    )
+
+    if (authError) {
+      return c.json({ message: 'Error updating auth user', error: authError.message }, 400);
+    }
+
+    // Update user in the users table (application-specific data)
+    const { error } = await supabase
+      .from('users')
+      .update({ email, username, phone, updated_at })
+      .eq('user_id', user_id);
+
+    if (error) {
+      return c.json({ message: 'Error updating user data', error: error.message }, 400);
+    }
+
+    return c.json({ message: 'User updated successfully', user_id, email, username, phone });
+  } catch (error) {
+    console.error('Unexpected error:', error);
+    return c.json({ message: 'Unexpected error', error: error }, 500);
+  }
+});
+
+// Delete user by Admin route
+user.delete('/deleteuser', async (c) => {
+  const supabase = createSupabaseClient(c);
+  const adminSupabase = adminSupabaseClient(c);
+  const { user_id } = await c.req.json();
+
+  try {
+    // Delete user from the users table (application-specific data)
+    const { data: deletedData, error: dbError } = await supabase
+      .from('users')
+      .delete()
+      .eq('user_id', user_id);
+
+    if (dbError) {
+      return c.json({ message: 'Error deleting user data from the database', error: dbError.message }, 400);
+    }
+
+    // Delete the user from the auth.users table (supabase authentication)
+    const { data: deletedAuthData, error: authError } = await adminSupabase.auth.admin.deleteUser(
+      user_id
+    )
+
+    if (authError) {
+      return c.json({ message: 'Error deleting user from auth', error: authError.message }, 400);
+    }
+
+    return c.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Unexpected error:', error);
     return c.json({ message: 'Unexpected error', error: error }, 500);
   }
 });
