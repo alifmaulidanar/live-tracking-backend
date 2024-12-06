3b7ad1a6baef908b8cedd881c4a8bc5d337e1d6c
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-06 22:16:25 +0700

    Added Supabase client for admin auth

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -10,3 +10,22 @@ export const createSupabaseClient = (c: any) => {
 
   return createClient(supabaseUrl, supabaseAnonKey);
 };
+
+export const adminSupabaseClient = (c: any) => {
+  const supabaseUrl = c.env.SUPABASE_URL;
+  const service_role_key = c.env.SERVICE_ROLE_KEY;
+
+  if (!supabaseUrl) {
+    throw new Error('Supabase URL is missing from environment variables');
+  }
+  if (!service_role_key) {
+    throw new Error('Role Key is missing from environment variables');
+  }
+
+  return createClient(supabaseUrl, service_role_key, {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false
+    }
+  })
+}
