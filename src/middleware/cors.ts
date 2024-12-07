82c3fb2aff57bd72fd25c1e82a5d6b61d33f2e83
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-07 20:25:02 +0700

    Updated CORS settings

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,5 +3,5 @@ import { cors } from 'hono/cors';
 export const corsMiddleware = cors({
   origin: '*',
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowHeaders: ['Content-Type', 'Authorization'],
+  allowHeaders: ['Content-Type', 'Authorization', 'user_id'],
 });
