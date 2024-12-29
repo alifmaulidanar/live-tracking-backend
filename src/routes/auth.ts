c36c3d9f78a54a1dd76e3295296e31d8c5305def
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-29 22:33:04 +0700

    Added new endpoint for admin login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -22,6 +22,35 @@ auth.post('/login', async (c) => {
   }
 });
 
+// Route for admin login
+auth.post('/adm/login/admin', async (c) => {
+  const supabase = createSupabaseClient(c);
+  const { email, password } = await c.req.json();
+
+  try {
+    const { data: adminData, error: adminError } = await supabase
+      .from('admins')
+      .select('email')
+      .eq('email', email)
+      .single();
+
+    if (adminError || !adminData) {
+      console.log("Admin not found");
+      return c.json({ message: 'Admin not found', error: adminError?.message }, 404);
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) {
+      console.log("Error logging in");
+      return c.json({ message: 'Error logging in', error: error.message }, 400);
+    }
+
+    return c.json(data);
+  } catch (error) {
+    return c.json({ message: 'Unexpected error', error: error }, 500);
+  }
+});
+
 // Route for logout
 auth.post('/logout', async (c) => {
   const supabase = createSupabaseClient(c);
